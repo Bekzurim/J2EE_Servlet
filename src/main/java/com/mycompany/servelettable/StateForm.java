4a8f317d175d637e9794ff1f18dc1d69d18ccedd
pedago <pedago@pcalb-mm0514.univ-jfc.fr>
2019-10-23 14:44:54 +0200

    La part 1 marche ! Le TP est pourri !

--- src/main/java/com/mycompany/servelettable/StateForm.java
+++ src/main/java/com/mycompany/servelettable/StateForm.java
@@ -1,24 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.mycompany.servelettable;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import simplejdbc.CustomerEntity;
-import simplejdbc.DAO;
-import simplejdbc.DataSourceFactory;
- 
-@WebServlet(name = "ShowClientInState", urlPatterns = {"/ShowClientInState"})
-public class ShowClient extends HttpServlet {
- 
+
+/**
+ *
+ * @author pedago
+ */
+@WebServlet(name = "StateForm", urlPatterns = {"/StateForm"})
+public class StateForm extends HttpServlet {
+
     /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
      *
      * @param request servlet request
      * @param response servlet response
@@ -26,47 +30,23 @@ public class ShowClient extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
- 
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<style>");
-            out.println("table, th, td {\n"+"border: 1px solid black;\n"+"}");
-            out.println("</style>");
-            out.println("<title>Servlet ShowClient</title>");
+            out.println("<title>Servlet StateForm</title>");            
             out.println("</head>");
             out.println("<body>");
-            try {   // Trouver la valeur du paramètre HTTP customerID
-                String val = request.getParameter("state");
-                if (val == null) {
-                    throw new Exception("Pas de paramètres transmis");
-                }
-                DAO dao = new DAO(DataSourceFactory.getDataSource());
-                List<CustomerEntity> customer = dao.customersInState(val);
-                if (customer == null) {
-                    throw new Exception("Client inconnu");
-                }        
-                out.println("<table style=\"width:20%\">");
-                out.println("<tr><th>ID</th>"+"<th>Nom</th>"+"<th>Adresse</th>");
-                for(int i=0;i<customer.size();i++)
-                {
-                    out.printf("<tr><td> %d </td>"+"<td> %s </td>"+"<td> %s </td></tr>",customer.get(i).getCustomerId(),customer.get(i).getName(),customer.get(i).getAddressLine1());
-                }
-                out.println("</tr>");
-            } catch (Exception e) {
-                out.printf("Erreur : %s", e.getMessage());
-            }
-            out.printf("<hr><a href='%s'>Retour au menu</a>", request.getContextPath());
+            out.println("<h1>Servlet StateForm at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
-        } catch (Exception ex) {
-            Logger.getLogger("servlet").log(Level.SEVERE, "Erreur de traitement", ex);
         }
     }
- 
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -77,10 +57,10 @@ public class ShowClient extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
- 
+
     /**
      * Handles the HTTP <code>POST</code> method.
      *
@@ -91,10 +71,10 @@ public class ShowClient extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
- 
+
     /**
      * Returns a short description of the servlet.
      *
@@ -103,6 +83,6 @@ public class ShowClient extends HttpServlet {
     @Override
     public String getServletInfo() {
         return "Short description";
-    }
- 
+    }// </editor-fold>
+
 }
